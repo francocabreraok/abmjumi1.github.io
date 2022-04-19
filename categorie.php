88e8d45c256d58aff865d54486392102a3adbc56
Franco Cabrera <franco.cabrera@gmail.com>
2022-04-19 16:09:36 -0300

    Creacion de VER PRODUCTOS dentro de CATEGORIAS, falta data

--- categorie.php
+++ categorie.php
@@ -3,7 +3,7 @@
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
   page_require_level(1);
-  
+
   $all_categories = find_all('categories')
 ?>
 <?php
@@ -12,6 +12,7 @@
    validate_fields($req_field);
    $cat_name = remove_junk($db->escape($_POST['categorie-name']));
    if(empty($errors)){
+      $date    = make_date();
       $sql  = "INSERT INTO categories (name)";
       $sql .= " VALUES ('{$cat_name}')";
       if($db->query($sql)){
@@ -67,22 +68,27 @@
                 <tr>
                     <th class="text-center" style="width: 50px;">#</th>
                     <th>Categorías</th>
+                    <th>Fecha de creacion</th>
                     <th class="text-center" style="width: 100px;">Acciones</th>
                 </tr>
             </thead>
             <tbody>
               <?php foreach ($all_categories as $cat):?>
                 <tr>
-                    <td class="text-center"><?php echo count_id();?></td>
-                    <td><?php echo remove_junk(ucfirst($cat['name'])); ?></td>
+                  <td class="text-center"><?php echo remove_junk(ucfirst($cat['id'])); ?></td>
+                  <td><?php echo remove_junk(ucfirst($cat['name'])); ?></td>
+                  <td><?php echo read_date($cat['date']); ?></td>
                     <td class="text-center">
                       <div class="btn-group">
-                        <a href="edit_categorie.php?id=<?php echo (int)$cat['id'];?>"  class="btn btn-xs btn-warning" data-toggle="tooltip" title="Editar">
-                          <span class="glyphicon glyphicon-edit"></span>
-                        </a>
-                        <a href="delete_categorie.php?id=<?php echo (int)$cat['id'];?>"  class="btn btn-xs btn-danger" data-toggle="tooltip" title="Eliminar">
-                          <span class="glyphicon glyphicon-trash"></span>
-                        </a>
+                        <a href="filter_product.php?id=<?php echo (int)$cat['id'];?>"  class="btn btn-xs btn-primary" data-toggle="tooltip" title="Ver Productos">
+                            <span class="glyphicon glyphicon-eye-open"></span>
+                          </a>
+                          <a href="edit_categorie.php?id=<?php echo (int)$cat['id'];?>"  class="btn btn-xs btn-warning" data-toggle="tooltip" title="Editar">
+                            <span class="glyphicon glyphicon-edit"></span>
+                          </a>
+                          <a href="delete_categorie.php?id=<?php echo (int)$cat['id'];?>"  class="btn btn-xs btn-danger" data-toggle="tooltip" title="Eliminar">
+                            <span class="glyphicon glyphicon-trash"></span>
+                          </a>
                       </div>
                     </td>
 
